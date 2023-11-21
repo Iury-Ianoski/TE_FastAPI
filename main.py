b38ba0c33642337daaa6d29e2c0b2adc17865efa
Iury R. Ianoski <122708817+Iury-Ianoski@users.noreply.github.com>
2023-11-21 09:39:37 -0300

    main.py creation

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class PessoaModel(BaseModel):
+   primeiro_nome : str
+   ultimo_nome : str
+   idade : int
+
+@app.get("/")
+def index():
+   return { "data" : "Testando FastAPI!" }
+
+@app.post("/hello")
+def helloPost(pessoa : PessoaModel):
+   return {
+       "data" : f'Hello {pessoa.primeiro_nome} {pessoa.ultimo_nome}, you are {pessoa.idade}!'
+   }
+
+
+@app.get("/hello/{name}")
+def helloName(name):
+   return { 
+"data" : f'Hello {name}!' 
+}
+
+@app.get("/quadrado/{num}")
+def quadrado(num: int):
+   return {
+       "data" : {
+           "num" : num,
+           "quadrado" : num ** 2
+       }
+   }
+
+@app.get("/pares")
+def listaPares(limit : Optional [int] = 20):
+   pares = []
+   for i in range(0, limit+1, 2):
+       pares.append(i)
+  
+   return {
+       "limit" : limit,
+       "pares" : pares
+   }
+
+
+
